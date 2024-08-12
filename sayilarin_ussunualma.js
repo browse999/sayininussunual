d9a0383f1b574d94a1b2bc3f724fe819374ab74d
Browser <92232900+browse999@users.noreply.github.com>
2024-08-12 19:44:11 +0300

    Add files via upload

---
+++ sayilarin_ussunualma.js
@@ -0,0 +1,13 @@
+let sayi1, sayi2 , üs
+
+
+
+
+
+sayi1 = Number(prompt('Sayi 1 Degerini giriniz'))
+
+sayi2 = Number(prompt('Sayi 2 değerini giriniz'))
+
+
+
+üs =  alert(`${sayi1} üssü ${sayi2}  = `+ sayi1**sayi2)
